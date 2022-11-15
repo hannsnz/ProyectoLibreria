27f934dba84c5f245e514a9f3e9faf4b6cc67c17
hannsnz <hannsnz@gmail.com>
2022-11-15 16:40:03 -0600

    Interfaz
    Interfaz de login completo

---
+++ SistemaLibreria/src/sistemalibreria/SistemaLibreria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemalibreria;
+
+/**
+ *
+ * @author 13-bb0502la
+ */
+public class SistemaLibreria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
