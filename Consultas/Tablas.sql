febc0022768898bf653ad0ad97314aef9899ae36
Rhene123 <renehc02@gmail.com>
2022-11-17 17:38:33 -0600

    actualización de tablas
    se agrego carrito y almacén

--- Consultas/Tablas.sql
+++ Consultas/Tablas.sql
@@ -63,6 +63,7 @@ create table Revista(
     volumen int not null,
     primary key (idRevista)
 );
+
 create table Ebook (
     idEbook int auto_increment not null,
     titulo varchar (60) not null,
@@ -76,11 +77,23 @@ create table Ebook (
     foreign key (idAutorE) references autor(idAutor)
 );
 
+
 create table Almacen (
 idAlmacen int auto_increment not null,
-
-
-
-
-
-
+dirCalle varchar (250) not null,
+dirNumero int not null,
+dirColonia varchar (250) not null,
+dirCP INT not null,
+dirCiudad varchar (250) not null,
+mail varchar (200) not null,
+telefono int not null,
+campus varchar (60) not null,
+primary key (idAlmacen)
+);
+create table Carrito(
+idCarrito int auto_increment not null,
+fechadecompra date not null,
+nombreLibros varchar (225),
+cantidadDelLibro int not null,
+primary key (idCarrito)
+);
